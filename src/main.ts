0a54884a9e5be1896d22df5cc77d18b9909db4f1
coderfish <18755634050@163.com>
2022-04-06 15:28:21 +0800

    feat: 阻止直接commit

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+console.log('测试阻止commit')
 createApp(App).use(store).use(router).mount('#app')
